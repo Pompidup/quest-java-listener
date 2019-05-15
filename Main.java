bcae8b37f8b7aff0ec59626ed5503914da485bfe
Pompidup <julienauvray28@gmail.com>
2019-05-15 10:43:33 +0200

    Add TransactionListener interface

--- Main.java
+++ Main.java
@@ -5,8 +5,23 @@ public class Main {
         Bank account = new Bank(1000);
 
         // TODO : create and use a TransactionListener interface with onComplete method
-        Transactions.withdraw(account, 100);
+        Transactions.withdraw(account, 100, new TransactionListener() {
+            @Override
+            public void onLoading() {
+                System.out.println("Please wait...");
+            }
 
-        System.out.printf("Your balance is: %d%n", account.getTotal());
+            @Override
+            public void onComplete(Bank account) {
+                System.out.printf("Your balance is: %d%n", account.getTotal());
+            }
+
+            @Override
+            public void onError(String error) {
+                System.out.printf("NOP : %s%n", error);
+            }
+        });
+
+
     }
 }
---
+++ TransactionListener.java
@@ -0,0 +1,9 @@
+public interface TransactionListener {
+
+    void onLoading();
+
+    void onComplete(Bank account);
+
+    void onError(String error);
+
+}
